e95d2c6acf6cda55c0065e9055d78b806e45ed32
John U <arjorb@gmail.com>
2024-02-14 12:11:39 +0200

    feat: setting up the codebase environment

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<div class="flex flex-col items-center">
+	<h1 class=" my-5 text-center text-2xl font-bold text-gray-700">Cinematic Stream AI</h1>
+	<Button>Coming soon</Button>
+</div>
