c63b1fb33e588fd6e59ca2ecf0eb20310a9f4f37
John U <arjorb@gmail.com>
2024-02-14 19:01:35 +0200

    feat: creating the homepage to get started:

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,10 @@
 	import { Button } from '$lib/components/ui/button';
 </script>
 
-<div class="flex flex-col items-center">
-	<h1 class=" my-5 text-center text-2xl font-bold text-gray-700">Cinematic Stream AI</h1>
-	<Button>Coming soon</Button>
+<div class="mt-72 flex w-full flex-col items-center">
+	<h1 class="my-5 text-center text-2xl font-bold text-gray-300">Cinematic Stream AI</h1>
+	<p class="my-5 w-[600px] text-center text-gray-500">
+		Step into a realm where imagination meets innovation with Cinematic Stream AI ✨
+	</p>
+	<Button>Get started</Button>
 </div>
